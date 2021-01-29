e949862cf4d34e16abcae24a42140d850a024917
Dean Herbert <pe@ppy.sh>
2021-01-29 18:42:45 +0900

    Simple repro

--- signalr-test/ViewController.cs
+++ signalr-test/ViewController.cs
@@ -17,23 +17,7 @@ namespace signalr_test
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
-
-            Task.Run(async () =>
-            {
-                var builder = new HubConnectionBuilder().WithUrl("https://dev.ppy.sh/spectator");
-                builder.ConfigureLogging(l => l.AddConsole());
-                var connection = builder.Build();
-                connection.Closed += Connection_Closed;
-                Console.WriteLine("Connecting...");
-                await connection.StartAsync();
-                Console.WriteLine("Connected!");
-            });
-        }
-
-        private Task Connection_Closed(Exception arg)
-        {
-            Console.WriteLine($"Connection closed with {arg}");
-            return Task.CompletedTask;
+            new HubConnectionBuilder().WithUrl("https://dev.ppy.sh/spectator").Build();
         }
 
         public override void DidReceiveMemoryWarning()
